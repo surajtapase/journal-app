8107474cca73f9238a551a012f19daa67840a364
surajtapase <surajtapase24@gmail.com>
2025-10-07 01:12:56 +0530

    Authentication added to users updated

--- src/main/java/app/springboot/JournalApp/controller/UserController.java
+++ src/main/java/app/springboot/JournalApp/controller/UserController.java
@@ -7,11 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 
 @RestController
 @RequestMapping("/user")
